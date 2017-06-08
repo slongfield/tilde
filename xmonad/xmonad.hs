55e41e14c418920c1dd2aea8dfab9c9c50a0f390
Stephen Longfield <slongfield@google.com>
2017-06-07 17:56:48 -0700

    Integrating work xmonad config.

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,8 +1,19 @@
 import XMonad
-import XMonad.Util.Run
+import XMonad.Hooks.Script
+import XMonad.Util.EZConfig
+import XMonad.Layout.NoBorders
+import XMonad.Config.Gnome
+import XMonad.Hooks.EwmhDesktops
 
-main = do
-       xmproc <- spawnPipe "/usr/bin/xmobar /home/slongfield/.xmonad/xmobarrc"    
-       xmonad $ defaultConfig { 
-                terminal = "xterm" 
-       }
+main = xmonad $ gnomeConfig {
+    -- XIM support is apparently causing terminal hangs...
+    terminal = "xterm-noxim",
+    -- layoutHook = smartBorders (layoutHook defaultConfig), --
+    -- layoutHook = desktopLayoutModifiers (Tall 1 0.03 0.05 ||| Full), -- To work better with Gnome
+    handleEventHook = fullscreenEventHook,
+    startupHook = do
+      execScriptHook "startup"
+      spawn "/usr/bin/xcompmgr" -- Nvidia issue: b/12995284
+}
+    `additionalKeys`
+    [((mod1Mask, xK_p), spawn "dmenu_run")]
